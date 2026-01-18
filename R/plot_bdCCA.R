8e689853b70eb8f799bb4744d9ede8877a47f56f
dpelegri <43083225+dpelegri@users.noreply.github.com>
2026-01-18 14:04:14 +0100

    Add crossed_coefs option to plot_bdCCA
    Introduces the crossed_coefs parameter to plot_bdCCA, allowing users to plot crossed canonical scores (CCAX_1 with CCAY_2, etc.). Updates function documentation and logic to support this new plotting mode.

--- R/plot_bdCCA.R
+++ R/plot_bdCCA.R
@@ -7,6 +7,8 @@
 #' @param filename string file name where dataset to normalize is stored.
 #' @param metadata  dataset with metadata file name where dataset to normalize is stored.
 #' @param pheno string column name with the pheno to be used in the plot
+#' @param crossed_coefs boolean plot crossed scores, CCAX_1 with CCAY_2, CCAX_2 with 
+#' CCAY_1 and CCAX_3 with CCAY_4
 #' @param column_id string (optional), metadata column name to be used to merge 
 #' data (rownames in xcoef and ycoef). If null, a merge of the datasets is 
 #' performed assuming that the data is sorted.
@@ -15,16 +17,16 @@
 #'    print ("Example in vignette")
 #'    
 #' @importFrom magrittr %>%
-plot_bdCCA <- function( hdf5_filename, metadata, pheno, 
+plot_bdCCA <- function( hdf5_filename, metadata, pheno, crossed_coefs = FALSE,
                         column_id = NULL, plot_filename = NULL  )
 {
-
+    
     # Check if file exists    
     if(!file.exists(hdf5_filename)){
         message("ERROR - File does not exists")
         return()
     }
-
+    
     # Check if pheno exists
     if(!pheno %in% colnames(metadata)){
         message("ERROR - Pheno variable can't be found in the metadata")
@@ -37,23 +39,22 @@ plot_bdCCA <- function( hdf5_filename, metadata, pheno,
         return()
     }
     
+    result_data <- bdgetDatasetsList_hdf5(filename = hdf5_filename, group = "Results")
+    
     # Open file and get data, all data is stored under SVD group
     h5f = H5Fopen(hdf5_filename)
-        # cor <- as.data.frame(h5f$Results$cor)
-        # colnames(cor) <- (h5f$Results$.xcoef_dimnames$`1`)[1:dim(cor)[1],1]
-        # rownames(cor) <- (h5f$Results$.xcoef_dimnames$`1`)[1:dim(cor)[1],1]
-        # 
-        xscores <- as.data.frame(h5f$Results$xscores)
-        colnames(xscores) <- sprintf("CCAX_%s",seq(1:dim(xscores)[2]))
-        yscores <- as.data.frame(h5f$Results$yscores)
-        colnames(yscores) <- sprintf("CCAY_%s",seq(1:dim(xscores)[2]))
+    xscores <- as.data.frame(h5f$Results$xscores)
+    yscores <- as.data.frame(h5f$Results$yscores)
     h5closeAll()
+    
+    colnames(xscores) <- sprintf("CCAX_%s",seq(1:dim(xscores)[2]))
+    colnames(yscores) <- sprintf("CCAY_%s",seq(1:dim(xscores)[2]))
     
     if(is.null(column_id))
         full_data <- cbind(metadata, xscores, yscores)
     else
         full_data <- merge(xscores, metadata, by.x = 0, by.y = column_id)
-
+    
     common_theme <- theme(
         axis.text = element_text(size = 28, face="bold"),
         axis.title = element_text(size = 28, face="bold"),
@@ -68,9 +69,14 @@ plot_bdCCA <- function( hdf5_filename, metadata, pheno,
         legend.title = element_blank()
     )
     
+    if(crossed_coefs == TRUE){
+        ccax <- c("CCAX_1", "CCAX_2", "CCAX_3")
+        ccay <- c("CCAY_2", "CCAY_1", "CCAY_4")
+    } else {
+        ccax <- c("CCAX_1", "CCAX_2", "CCAX_3")
+        ccay <- c("CCAY_1", "CCAY_2", "CCAY_3")
+    }
     
-    ccax <- c("CCAX_1", "CCAX_2", "CCAX_3")
-    ccay <- c("CCAY_1", "CCAY_2", "CCAY_3")
     full_data[,pheno] <- as.factor(full_data[,pheno])
     
     p1 <- full_data %>%
@@ -94,14 +100,14 @@ plot_bdCCA <- function( hdf5_filename, metadata, pheno,
         common_theme +
         guides(colour = guide_legend(override.aes = list(size = 16)))
     
-    
     if(!is.null(plot_filename)) {
         fname <- plot_filename
         png(filename = fname, width = 1815, height = 900, units = "px")
-            grid.arrange( p1, p2, p3, nrow = 1 )
+        grid.arrange( p1, p2, p3, nrow = 1 )
         dev.off()
     } 
     
+    print(grid.arrange( p1, p2, p3, nrow = 1 ))
     return( grid.arrange( p1, p2, p3, nrow = 1 ))
     
 }
